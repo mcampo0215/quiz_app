7580788fbfa1cae24ceee53fd9391f64e468cb10
mcampo0215 <matthewcampoverde@gmail.com>
2026-02-12 11:36:53 -0500

    Added questions class along with different questions and answer choices

---
+++ lib/data/questions.dart
@@ -0,0 +1,69 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'If you could only eat one of these forever, which is statistically the most popular comfort food in the U.S.?'
+    ,
+    [
+      'Sushi',
+      'Pizza',
+      'Tacos',
+      'Ramen',
+    ]
+  ),
+
+  QuizQuestion(
+    'Which country is home to the most time zones?'
+    ,
+    [
+      'Russia',
+      'United States',
+      'France',
+      'Australia',
+    ]
+  ),
+
+  QuizQuestion(
+    'What is the only mammal capable of true flight?'
+    ,
+    [
+      'Flying squirrel',
+      'Sugar glider',
+      'Bat',
+      'Colugo',
+    ]
+  ),
+
+  QuizQuestion(
+    'Which TV show featured the fictional paper company Dunder Mifflin'
+    ,
+    [
+      'Parks and Recreation',
+      'Brooklyn Nine-Nine',
+      'The Office',
+      'Superstore',
+    ]
+  ),
+
+  QuizQuestion(
+    'What was the first social media platform to reach 1 billion users?'
+    ,
+    [
+      'MySpace',
+      'Instagram',
+      'Facebook',
+      'Twitter',
+    ]
+  ),
+
+  QuizQuestion(
+    'You see a house with four walls. All the walls face south, and a bear walks by. What color is the bear?'
+    ,
+    [
+      'Brown'
+      'Black',
+      'White',
+      'Gray',
+    ]
+  ),
+];
