bf3e63320a2305214b30043029f66201079283c6
mcampo0215 <matthewcampoverde@gmail.com>
2026-03-03 12:14:00 -0500

    Added results screen and switch to results screen once quiz is completed

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -4,8 +4,10 @@ import 'package:quiz_app/data/questions.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectedAnswer});
 
+  final void Function(String answer) onSelectedAnswer;
+  @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
@@ -13,7 +15,8 @@ class QuestionsScreen extends StatefulWidget {
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
-  void answerQuestion() {
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectedAnswer(selectedAnswer);
     setState(() {
       currentQuestionIndex++;
     });
@@ -40,7 +43,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((item) {
               return AnswerButton(answerText: item,
-              onTap: answerQuestion);
+              onTap: (){
+                answerQuestion(item);
+            });
             }),
         ],),
       ),
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,24 +1,39 @@
 //quiz.dart
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
+  @override
   State<Quiz> createState() {
     return _QuizState();
   }
 }
 
 class _QuizState extends State<Quiz> {
+  final List<String> selectedAnswers = [];
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState((){
+        activeScreen = ResultsScreen(chosenAnswers: selectedAnswers);
+      });
+    }
+  }
+
   Widget? activeScreen;
   void switchScreen() {
     setState(() {
-      activeScreen = QuestionsScreen();
+      activeScreen = QuestionsScreen(onSelectedAnswer: chooseAnswer);
     });
   }
 
+  @override
   void initState() {
     activeScreen = StartScreen(switchScreen);
     super.initState();
